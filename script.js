8754a2c69f0d83b3c6055b8b264e2f7ed46889c9
sourav512 <55541916+sourav512@users.noreply.github.com>
2021-08-25 01:35:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ function redirectTo() {
     let contact = document.getElementById('contact');
     let countryCode = document.getElementById('countryCode');
     location.replace('https://wa.me/'+countryCode.value+contact.value);
+    contact.value = '';
     // console.log('https://wa.me/'+countryCode.value+contact.value);
 }
