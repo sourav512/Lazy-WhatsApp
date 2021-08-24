979a02a0bbcc0aa379357c466a247aefaa3d5acf
sourav512 <55541916+sourav512@users.noreply.github.com>
2021-08-25 01:28:37 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function redirectTo() {
+    let contact = document.getElementById('contact');
+    let countryCode = document.getElementById('countryCode');
+    location.replace('https://wa.me/'+countryCode.value+contact.value);
+    // console.log('https://wa.me/'+countryCode.value+contact.value);
+}
