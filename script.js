db0633a18ccf5d1d41d0ed719b5598e104e76690
sourav512 <souravbarh2000@gmail.com>
2022-08-17 20:45:02 +0530

    number issue solved

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function redirectTo() {
     let contact = document.getElementById('contact');
+    contact = contact.value.replaceAll(" ","");
+    contact = contact.slice(3);
+    // console.log(contact)            
     let countryCode = document.getElementById('countryCode');
-    location.replace('https://wa.me/'+countryCode.value+contact.value);
+    location.replace('https://wa.me/'+countryCode.value+contact);
     contact.value = '';
     // console.log('https://wa.me/'+countryCode.value+contact.value);
 }
+
+
